198fbfe82d7edfb54c083c3a9d5defb68dd2f359
Thomas Zuberbühler <thomas.zuberbuehler@gmail.com>
2019-09-22 19:59:22 -0300

    Create read-sources.py

---
+++ python/read-sources.py
@@ -0,0 +1,14 @@
+#!c:/program files/arcgis/pro/bin/python/envs/arcgispro-py3/python.exe
+# -*- coding: utf-8 -*-
+
+# Read sources in current ArcGIS Pro project added to a map.
+#
+# Author: Thomas Zuberbuehler (thomas.zuberbuehler@gmail.com)
+
+aprx = arcpy.mp.ArcGISProject("Current")
+maps = aprx.listMaps()
+for map in maps:
+  layers = map.listLayers()
+  sorted_layers = sorted(layers, key=lambda layer: layer.dataSource)
+  for layer in layers:
+    print (layer.dataSource)
